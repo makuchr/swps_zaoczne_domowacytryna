61a34590b9385fad64568f5d674a4d9c4cc5ec09
makuchr <130873089+makuchr@users.noreply.github.com>
2025-11-30 14:29:52 +0100

    Init

---
+++ Arbuz/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Let's play Rock-Paper-Scissors!");
+
+Console.WriteLine("Choose your sign, pleyr 1 (rock/paper/scissors):");
+string firstSign = Console.ReadLine()!;
+
+Console.WriteLine("Choose your sign, pleyr 2 (rock/paper/scissors):");
+string secondSign = Console.ReadLine()!;
+
+if (firstSign.Equals(secondSign, StringComparison.CurrentCultureIgnoreCase))
+{
+    Console.WriteLine("It's a draw!");
+}
+else if ((firstSign.Equals("rock", StringComparison.CurrentCultureIgnoreCase) && secondSign.Equals("scissors", StringComparison.CurrentCultureIgnoreCase))
+    || (firstSign.Equals("scissors", StringComparison.CurrentCultureIgnoreCase) && secondSign.Equals("paper", StringComparison.CurrentCultureIgnoreCase))
+    || (firstSign.Equals("paper", StringComparison.CurrentCultureIgnoreCase) && secondSign.Equals("rock", StringComparison.CurrentCultureIgnoreCase)))
+{
+    Console.WriteLine("First player won!");
+}
+else
+{
+    Console.WriteLine("Second player won!");
+}
