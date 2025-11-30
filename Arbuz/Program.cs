99c56332679a12137e2ebbbc99362bd988e1addc
makuchr <rmakuch@onet.pl>
2025-11-30 16:01:15 +0100

    FinalCode

--- Arbuz/Program.cs
+++ Arbuz/Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.WriteLine("Let's play Rock Paper Scissors!");
 
-string firstSign = GetCorrectSign("Player 1");
+string sign = GetCorrectSign("Player 1");
 string secondSign = GetCorrectSign("Player 2");
 
-if (firstSign.Equals(secondSign, StringComparison.OrdinalIgnoreCase))
+if (sign.Equals(secondSign, StringComparison.OrdinalIgnoreCase))
 {
     Console.WriteLine("It's a tie!");
 }
-else if ((firstSign == "rock" && secondSign == "scissors")
-    || (firstSign == "paper" && secondSign == "rock")
-    || (firstSign == "scissors" && secondSign == "paper"))
+else if ((sign == "rock" && secondSign == "scissors")
+    || (sign == "paper" && secondSign == "rock")
+    || (sign == "scissors" && secondSign == "paper"))
 {
     Console.WriteLine("First player won!");
 }
@@ -24,14 +24,14 @@ else
 string GetCorrectSign(string playerName)
 {
     Console.WriteLine($"{playerName}, provide sign (rock/paper/scissors)");
-    string firstSign = Console.ReadLine()!;
+    string sign = Console.ReadLine()!;
     // czy podany znak NIE jest jednym z oczekiwanych?
-    while (!(firstSign == "rock" || firstSign == "paper" || firstSign == "scissors"))
+    while (!(sign == "rock" || sign == "paper" || sign == "scissors"))
     {
         // wyświetl ze znak jest niepoprawny
         Console.WriteLine("Provide correct sign! (rock/paper/scissors)");
         // pozwól poprawić swoje zachowanie (poda ponownie znak)
-        firstSign = Console.ReadLine()!;
+        sign = Console.ReadLine()!;
     }   
-    return firstSign;
+    return sign;
 }
