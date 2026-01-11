e09f8d64e329d8100eb7c69a5d21527c0c19ff0f
makuchr <rmakuch@onet.pl>
2026-01-11 14:45:07 +0100

    Points

--- Arbuz/Program.cs
+++ Arbuz/Program.cs
@@ -1,45 +1,61 @@
 ﻿List<string> allowedSigns = ["rock", "paper", "scissors"];
 
-Console.WriteLine("Let's play Rock Paper Scissors!");
+int firstPlayerPoints = 0;
+int secondPlayerPoints = 0;
 
-string sign = GetCorrectSign("Player 1");
-string secondSign = GetRandomSign("Player 2");
+Console.WriteLine("Let's play Rock Paper Scissors, ok?");
+while (true)
+{
+    string firstSign = GetCorrectSign("Player 1");
+    string secondSign = GetRandomSign("Player 2");
 
-int secondSignIndex = allowedSigns.IndexOf(secondSign);
-int indexOfSignWinningWithSecondSign = (secondSignIndex+1) % allowedSigns.Count;
-int firstSignIndex = allowedSigns.IndexOf(sign);
+    // 1. Znajdź indeks znaku podanego przez drugą osobę -> x
+    int secondSignIndex = allowedSigns.IndexOf(secondSign);
+    // 2. Wylicz indeks znaku, który wygrywa ze znakiem drugiej osoby -> (x + 1) % length
+    int indexOfSignWinningWithSecondSign = (secondSignIndex + 1) % allowedSigns.Count;
+    // 3. Znajdź indeks znaku podanego przez pierwszą osobę -> y
+    int firstSignIndex = allowedSigns.IndexOf(firstSign);
 
-if (sign.Equals(secondSign, StringComparison.OrdinalIgnoreCase))
-{
-    Console.WriteLine("It's a tie!");
-}
-else if (firstSignIndex==indexOfSignWinningWithSecondSign)
-{
-    Console.WriteLine("First player won!");
-}
-else
-{
-    Console.WriteLine("Second player won!");
+
+    if (firstSign.Equals(secondSign, StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("It's a tie!");
+    }
+    // 4. Sprawdź czy indeks znaku pierwszej osoby jest równy indeksowi znaku wygrywającego
+    //    ze znakiem drugiej osoby -> y == (x + 1) % l
+    else if (firstSignIndex == indexOfSignWinningWithSecondSign)
+    {
+        Console.WriteLine("First player won!");
+        firstPlayerPoints+=1;
+    }
+    else
+    {
+        Console.WriteLine("Second player won!");
+        secondPlayerPoints+=1;
+    }
+    Console.WriteLine($"First player: {firstPlayerPoints}");
+    Console.WriteLine($"First player: {secondPlayerPoints}");
 }
 
 string GetCorrectSign(string playerName)
 {
-    Console.WriteLine($"{playerName}, provide sign ({string.Join("/",allowedSigns)})");
+    Console.WriteLine($"{playerName}, provide sign ({string.Join("/", allowedSigns)})");
     string sign = Console.ReadLine()!;
     // czy podany znak NIE jest jednym z oczekiwanych?
     while (!allowedSigns.Contains(sign))
     {
         // wyświetl ze znak jest niepoprawny
-        Console.WriteLine($"Provide correct sign! ({string.Join("/",allowedSigns)})");
+        Console.WriteLine($"Provide correct sign! ({string.Join("/", allowedSigns)})");
         // pozwól poprawić swoje zachowanie (poda ponownie znak)
         sign = Console.ReadLine()!;
-    }   
+    }
+
     return sign;
 }
 
 string GetRandomSign(string playerName)
 {
-    int index=Random.Shared.Next(allowedSigns.Count);
+    int index = Random.Shared.Next(allowedSigns.Count);
     string sign = allowedSigns[index];
     Console.WriteLine($"{playerName} chosen {sign}");
     return sign;
